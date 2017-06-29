ca459ccd01a41e0880935f7a28e50fb8d6576fc7
Ranjangsk <gskranjan@gmail.com>
2017-06-29 16:52:10 +0530

    asd

--- server/mongoose.js
+++ server/mongoose.js
@@ -4,6 +4,12 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/todoapp');
 
+let db = {
+  localhost: 'mongodb://localhost:27017/todoApp',
+  mlab: 'mongodb://ranjan:1234@ds149268.mlab.com:49268/todoapp'
+};
+
+
+mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
 module.exports.mongoose=mongoose;
